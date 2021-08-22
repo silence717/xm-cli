54bee02170972d740ef0ce83608047073e64c8b9
fang.yang <yangfang2028@126.com>
2021-08-22 18:28:29 +0800

    :sparkles: Add core and utils package

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,29 @@
 #! /usr/bin/env node
+const lib = require('xm-cli-libs')
+// 注册一个命令  xm-cli init
+const argv = require('process').argv;
+const command = argv[2];
+const options = argv.slice(3)
 
-console.log('hello xm-cli');
+if (options.length > 1) {
+    let [option, param] = options
+    option = option.replace('--', '')
+
+    if (command) {
+        if (lib[command]) {
+            lib[command]({ option, param })
+        } else {
+            console.log('invalid command')
+        }
+    } else {
+        console.log('please input command')
+    }
+}
+
+// 实现参数解析  --version 和 init  --name
+if (command.startsWith('--') || command.startsWith('-')) {
+    const globalOption = command.replace(/--|-/g, '');
+    if (globalOption === 'version' || globalOption === 'V') {
+        console.log('1.0.0')
+    }
+}
