a18b7232f9742e928f5d43e1c11a366952d2b9fd
fang.yang <yangfang2028@126.com>
2021-10-04 14:58:24 +0800

    ✨ Develop program register

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 'use strict'
 module.exports = core;
 const path = require('path')
@@ -5,11 +7,15 @@ const semver = require('semver')
 const colors = require('colors/safe')
 const userHome = require('user-home')
 const pathExists = require('path-exists').sync
+
+const commander = require('commander')
 const log = require('@how-xm/log')
 const pkg = require('../package.json')
 const constant = require('./constant')
 
 let args
+
+const program = new commander.Command()
 
 async function core() {
     try {
@@ -17,12 +23,49 @@ async function core() {
         checkNodeVersion()
         checkRoot()
         checkUserHome()
-        checkInputArgs()
+        // checkInputArgs()
         checkEnv()
         await checkGlobalUpdate()
+        registerCommand()
     } catch (e) {
         log.error(e.message)
     }
+}
+
+function registerCommand() {
+    program
+        .name(Object.keys(pkg.bin)[0])
+        .usage('<command> [options]')
+        .version(pkg.version)
+        .option('-d --debug', '是否开启调试模式', false)
+
+    // 开启debug模式
+    program.on('option:debug', function () {
+        if (program.debug) {
+            process.env.LOG_LEVEL = 'verbose';
+        } else {
+            process.env.LOG_LEVEL = 'info';
+        }
+        log.level = process.env.LOG_LEVEL
+        log.verbose('test')
+    })
+
+    // 对未知命令的监听
+    program.on('command:*', function (obj) {
+        const availableCommands = program.commands.map(cmd => cmd.name())
+        console.log(colors.red('未知的命令：'+ obj[0]))
+        if (availableCommands.length > 0) {
+            console.log(colors.green('可用命令：' + availableCommands.join(',')))
+        }
+    })
+
+    if (program.args && program.args.length < 1) {
+        program.outputHelp()
+        console.log()
+    }
+    console.log(program)
+
+    program.parse(process.argv)
 }
 
 async function checkGlobalUpdate() {
